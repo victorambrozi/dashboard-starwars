0b366dd802ff674bd1326a6740b0295f15ade59e
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-04-01 18:28:21 -0300

    request made to fill in the cards

---
+++ script/main.js
@@ -0,0 +1,25 @@
+const personagensContador = document.querySelector("#personagens");
+const luasContador = document.querySelector("#luas");
+const planetasContador = document.querySelector("#planetas");
+const navesContador = document.querySelector("#naves");
+
+function request(param) {
+    return axios.get(`https://swapi.dev/api/${param}`)
+}
+
+function preencherContadores() {
+    Promise.all([
+        request("people/"),
+        request("vehicles/"),
+        request("planets/"),
+        request("starships/")])
+        .then(function (results) {
+            personagensContador.innerHTML = results[0].data.count
+            luasContador.innerHTML = results[1].data.count
+            planetasContador.innerHTML = results[2].data.count
+            navesContador.innerHTML = results[3].data.count
+        });
+};
+
+preencherContadores();
+
